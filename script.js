4c962e56a52c2f4992ed2d70b2a336b1fee3be34
Amanda Cristalino <amacristalino@gmail.com>
2023-05-07 14:51:34 -0300

    trabalho

---
+++ script.js
@@ -0,0 +1,9 @@
+function manha (){
+    document.getElementById("trocarimg").src="manha.jpg";
+}
+function tarde (){
+    document.getElementById("trocarimg").src="tarde.jpg";
+}
+function noite (){
+    document.getElementById("trocarimg").src="noite.jpg";
+}
